d7e37d0ff11b06be23bf5b03c5f970cd73b4c7bd
Robin667 <robin.turpin8@gmail.com>
2022-03-03 23:03:20 +0100

    change name Pokemon to PokemonCard

--- pokedex-app/src/components/pages/PokemonsDashBoard.jsx
+++ pokedex-app/src/components/pages/PokemonsDashBoard.jsx
@@ -1,75 +1,74 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
-import logo from '../../logo.svg'
-import Pokemon from '../pokemonList/PokemonCard'
-import './pokemonDashBoard.css'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import logo from "../../logo.svg";
+import PokemonCard from "../pokemonList/PokemonCard";
+import "./pokemonDashBoard.css";
 
-function PokemonsDashBoard () {
-
-  const [pokemons, setPokemons] = useState([])
-  const [loading, setLoading] = useState(true)
+function PokemonsDashBoard() {
+  const [pokemons, setPokemons] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   const api = axios.create({
-    baseURL: 'https://pokeapi.co/api/v2',
+    baseURL: "https://pokeapi.co/api/v2",
     headers: {
-      'Content-Type': 'application/json',
-      Accept: 'application/json'
+      "Content-Type": "application/json",
+      Accept: "application/json",
     },
-    timeout: 10000
-  })
+    timeout: 10000,
+  });
 
   const getPokemonsList = async () => {
     let pokemonArray = [];
     for (let i = 1; i < 152; i++) {
-      pokemonArray.push(await getPokemonsById(i))
+      pokemonArray.push(await getPokemonsById(i));
     }
-    setLoading(true)
-    setPokemons(pokemonArray)
-    setLoading(false)
-  }
-  
+    setLoading(true);
+    setPokemons(pokemonArray);
+    setLoading(false);
+  };
+
   const getPokemonsById = async (id) => {
     try {
-      const res = await api.get(`/pokemon/${id}`)
-      return res
+      const res = await api.get(`/pokemon/${id}`);
+      return res;
     } catch (error) {
-      console.error(error)
+      console.error(error);
     }
-  }
-  
+  };
+
   useEffect(() => {
     const getData = async () => {
-      getPokemonsList()
-    }
-    getData()
-  }, [])
+      getPokemonsList();
+    };
+    getData();
+  }, []);
 
   if (loading) {
     return (
       <div>
-        <img src={logo} className='App-logo' alt='logo' /> Fetching Pokemon
+        <img src={logo} className="App-logo" alt="logo" /> Fetching Pokemon
       </div>
-    )
+    );
   }
 
   return (
-    <div className='app-container'>
+    <div className="app-container">
       <h1>PokeDex</h1>
       <div className="pokemon-container">
         <div className="all-container">
           {pokemons.map((pokemon, index) => (
-              <Pokemon 
+            <PokemonCard
               id={pokemon.data.id}
               name={pokemon.data.name}
               image={pokemon.data.sprites.other.dream_world.front_default}
               type={pokemon.data.types[0].type.name}
               key={index}
-              />
+            />
           ))}
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default PokemonsDashBoard
+export default PokemonsDashBoard;
